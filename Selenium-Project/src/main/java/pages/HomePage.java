a1e176e0cecf397264d245fef656902447299cc2
Sadafbakhtavar <sadafbakhtavar@gmail.com>
2022-08-19 12:53:51 -0400

    adding a new project

---
+++ Selenium-Project/src/main/java/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+    @FindBy(id = "_idgpn_5") private WebElement username;
+
+}
