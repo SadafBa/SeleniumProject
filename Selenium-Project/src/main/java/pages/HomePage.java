92ec40096d6421d07ccbd689a8a43e9b36f80b8e
Sadafbakhtavar <sadafbakhtavar@gmail.com>
2022-08-21 12:40:43 -0400

    added a line

--- Selenium-Project/src/main/java/pages/HomePage.java
+++ Selenium-Project/src/main/java/pages/HomePage.java
@@ -1,9 +1,21 @@
 package pages;
 
+import Browser.Browser;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import static Browser.Browser.getPropertyValue;
+
 public class HomePage {
-    @FindBy(id = "_idgpn_5") private WebElement username;
+    @FindBy(id = "_idgpn_5") private WebElement userNameElement;
+    @FindBy(id = "") private WebElement passwordElement;
+    @FindBy(id = "") private WebElement loginButton;
+
+
+    public void logIn(){
+        userNameElement.sendKeys(getPropertyValue("userName"));
+        passwordElement.sendKeys(getPropertyValue("password"));
+        loginButton.click();
+    }
 
 }
